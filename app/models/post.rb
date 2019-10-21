d80fa24937b5ee7afd8d7ad9c0d0bb533df61800
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-21 16:11:37 -0600

    adjustment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   has_one :users
   has_many :comments
 
+  #belongs_to :user
+  #has_many :comments
+
   validates :title, length: { maximum: 250 }
   validates_presence_of :title, message: 'Please add title'
 
