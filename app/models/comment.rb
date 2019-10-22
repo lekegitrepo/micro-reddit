0768563df480b4bf4c39d8798a286c3f6e50f21d
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-10-22 11:24:33 -0400

    updated comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :comment, presence: true, length: { maximum: 250 }
+  validates :name, length: { maximum: 250 }
+  validates_presence_of :name, message: 'Please add name'
+
+  validates_presence_of :body, message: 'Please add a body text'
 end
