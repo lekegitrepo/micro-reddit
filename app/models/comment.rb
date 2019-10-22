51a30b468e4c4b8f955a2ec3e6e836119b74b56d
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-22 09:45:08 -0600

    modify commenter's name length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :name, length: { maximum: 250 }
+  validates :name, length: { maximum: 20 }
   validates_presence_of :name, message: 'Please add name'
 
   validates_presence_of :body, message: 'Please add a body text'
